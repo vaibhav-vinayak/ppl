b08aaa8bb9973354f1294c02b863e45f461cdff1
Vaibhav Vinayak <vaibhav.vinayak@outlook.com>
2022-05-12 23:40:52 +0530

    navbar modifications

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,11 +1,27 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+const navLinkClass = ({ isActive }) => {
+  return `nav-link${isActive ? " active" : ""}`;
+};
 
 const Navbar = () => (
-  <nav className="navbar navbar-light bg-light">
-    <div className="container-fluid">
-      <Link className="navbar-brand" to="/">
-        Navbar
-      </Link>
+  <nav className="navbar navbar-expand navbar-light bg-light">
+    <div className="container">
+      <NavLink className="navbar-brand" to="/">
+        PPL
+      </NavLink>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <NavLink className={navLinkClass} to="/login">
+            Login
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className={navLinkClass} to="/signup">
+            Signup
+          </NavLink>
+        </li>
+      </ul>
     </div>
   </nav>
 );
