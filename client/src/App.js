a3b8ee5ce893c56c68952f191ed7a38816ef439c
Vaibhav Vinayak <vaibhav.vinayak@outlook.com>
2022-05-12 23:41:13 +0530

    updated app root

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+const Navbar = lazy(() => import("components/Navbar"));
 const Home = lazy(() => import("pages/Home"));
 const Login = lazy(() => import("pages/Auth/Login"));
+const Signup = lazy(() => import("pages/Auth/Signup"));
 
 const App = () => (
   <Router>
     <Suspense fallback={null}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
     </Suspense>
   </Router>
 );
