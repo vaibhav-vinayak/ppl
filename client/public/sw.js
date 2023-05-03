e29d59a6fd7f7f54b06d5ea9a4086feea6c95fef
vaibhav.vinayak@outlook.com <Vaibhav Vinayak>
2023-05-03 18:57:56 +0530

    commenting service worker for some time

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,31 +1,31 @@
-if (caches) {
-  const CACHE_NAME = "ppl-cache";
-  const VENDOR_RESOURCES = ["/bootstrap.bundle.min.js", "/bootstrap.min.css"];
-  const RESOURCES = [...VENDOR_RESOURCES];
+// if (caches) {
+//   const CACHE_NAME = "ppl-cache";
+//   const VENDOR_RESOURCES = ["/bootstrap.bundle.min.js", "/bootstrap.min.css"];
+//   const RESOURCES = [...VENDOR_RESOURCES];
 
-  /**
-   * @event Install
-   */
-  self.addEventListener("install", (e) =>
-    e.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => cache.addAll(RESOURCES))
-    )
-  );
+//   /**
+//    * @event Install
+//    */
+//   self.addEventListener("install", (e) =>
+//     e.waitUntil(
+//       caches.open(CACHE_NAME).then((cache) => cache.addAll(RESOURCES))
+//     )
+//   );
 
-  /**
-   * @event Fetch
-   */
-  self.addEventListener("fetch", (e) =>
-    e.respondWith(
-      caches.open(CACHE_NAME).then((cache) =>
-        cache
-          .match(e.request)
-          .then((res) => res || fetch(e.request))
-          .then((res) => {
-            cache.put(e.request, res.clone());
-            return res;
-          })
-      )
-    )
-  );
-}
+//   /**
+//    * @event Fetch
+//    */
+//   self.addEventListener("fetch", (e) =>
+//     e.respondWith(
+//       caches.open(CACHE_NAME).then((cache) =>
+//         cache
+//           .match(e.request)
+//           .then((res) => res || fetch(e.request))
+//           .then((res) => {
+//             cache.put(e.request, res.clone());
+//             return res;
+//           })
+//       )
+//     )
+//   );
+// }
